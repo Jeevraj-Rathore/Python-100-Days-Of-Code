1e37b7380bc3c382dcf4c64a10ad9e61146d78f5
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-07-28 22:15:50 +0530

    Day 1

---
+++ Day 1/main.py
@@ -0,0 +1,32 @@
+print("Hello World")
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# String Manipulation
+print("Hello World!\nHello World!")
+
+# String Concatenation --> merges multiple string into one
+print("Hello " + "Jeevraj")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "'"+"'" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# Input Function
+print("Hello " + input("What is your name? ") + "!")
+
+# Printing the lenght of the input
+name = input("What is your name? ")
+print(len(name))
+
+# Qusetion: Exchange the values of a and b
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+c = a
+a = b
+b = c
+print("a = "+ a)
+print("b = " + b)
