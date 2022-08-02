7a41baec85c5af31b6ea7e0907fbf18173bea9ca
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-08-02 19:06:34 +0530

    Day 6

---
+++ Day 6/functions.py
@@ -0,0 +1,26 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# Defining our own function
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# Calling a function
+my_function()
+
+# Reeborg's World :-
+# def turn_right():
+#   turn_left()
+#   turn_left()
+#   turn_left()
+
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
---
+++ Day 6/indentation.py
@@ -0,0 +1,12 @@
+sky = input("How sky looks like? ")
+
+def my_functon():
+    if sky == "clear":
+        print("blue")
+    elif sky == "cloudy":
+        print("grey")
+    else:
+        print("hello")
+print("world")
+
+my_functon()
